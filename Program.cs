d1a75f0ea47188ec76b4e4922575f8377894482f
Johan Gil <Stivengil95>
2022-08-16 19:16:27 -0500

    Se suben los archivos

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+bool encendido = true;
+int opciones = 0;
+string usuario = "";
+
+
+Console.WriteLine("Bienvenido al banco ingese su usuario");
+usuario = Console.ReadLine();
+
+
+
+while (encendido == true)
+{
+   Console.WriteLine("Bienvenido");
+
+   switch(opciones)
+    {
+        case 1:
+
+          break;
+
+
+        case 2:
+
+          break;
+
+
+        case 3:
+
+          break;
+
+
+     
+    }
+
+
+}
+
+
+   
+
+
+
